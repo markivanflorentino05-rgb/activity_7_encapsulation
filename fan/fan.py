87c09d0709302e876c8657784be5722d2b5fcba1
Mark Ivan F. Florentino <markivanflorentino05@gmail.com>
2026-06-04 01:42:15 +0800

    Implement __init__ with default values

--- fan/fan.py
+++ fan/fan.py
@@ -3,8 +3,8 @@ class Fan:
     MEDIUM = 2
     FAST = 3
 
-    def __init__(self):
-        self.__speed = None
-        self.__radius = None
-        self.__color = None
-        self.__on = None
+    def __init__(self, speed=SLOW, radius=5.0, color="blue", on=False):
+        self.__speed = speed
+        self.__radius = radius
+        self.__color = color
+        self.__on = on
