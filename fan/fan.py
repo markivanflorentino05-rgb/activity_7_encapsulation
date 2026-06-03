77c4db43d7b27ee8947e196a9ceea12d57f22bf3
Mark Ivan F. Florentino <markivanflorentino05@gmail.com>
2026-06-04 01:44:55 +0800

    Add setter methods (without validation yet)

--- fan/fan.py
+++ fan/fan.py
@@ -11,12 +11,16 @@ class Fan:
 
     def get_speed(self):
         return self.__speed
-
     def get_radius(self):
         return self.__radius
-
     def get_color(self):
         return self.__color
-
     def is_on(self):
         return self.__on
+
+    def set_speed(self, speed):
+        self.__speed = speed
+    def set_radius(self, radius):
+        self.__radius = radius
+    def set_color(self, color):
+        self.__color = color
