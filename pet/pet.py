9cd5e34dd143255bef98c6332a7fb214e5628dc8
Mark Ivan F. Florentino <markivanflorentino05@gmail.com>
2026-06-04 02:10:36 +0800

    Add setter methods

--- pet/pet.py
+++ pet/pet.py
@@ -3,3 +3,12 @@ class Pet:
         self.__name = name
         self.__animal_type = animal_type
         self.__age = age
+
+    def set_name(self, name):
+        self.__name = name
+
+    def set_animal_type(self, animal_type):
+        self.__animal_type = animal_type
+
+    def set_age(self, age):
+        self.__age = age
