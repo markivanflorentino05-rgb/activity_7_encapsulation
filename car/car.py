f550b2303c0a9bc296398d1b00287d3403450c29
Mark Ivan F. Florentino <markivanflorentino05@gmail.com>
2026-06-04 02:01:35 +0800

    Add getter methods

--- car/car.py
+++ car/car.py
@@ -3,3 +3,12 @@ class Car:
         self.__year_model = year_model
         self.__make = make
         self.__speed = 0
+
+    def get_year_model(self):
+        return self.__year_model
+
+    def get_make(self):
+        return self.__make
+
+    def get_speed(self):
+        return self.__speed
